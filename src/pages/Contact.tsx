ade32786105a141caccd99c4c517ed0d98fe0113
Steven Kebila <bongamke@sheridancollege.ca>
2025-05-08 16:05:34 -0400

    Update Contact.tsx
    Added Form API

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -33,12 +33,10 @@ const Contact = () => {
       
       // This is where you would make an API call to your email sending service
       // Example of what that might look like:
-      const response = await fetch('/api/send-email', {
+      const response = await fetch('https://formspree.io/f/xovdwzdv', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-          to: 'hello@launchab.com',
-          subject: 'New Client Inquiry from Launchab Website',
           name: data.name,
           email: data.email,
           phone: data.phone,
